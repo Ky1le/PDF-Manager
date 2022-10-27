2dbecbcb8b1cadea4dced6f522b365b75298d657
Ky1le <Doener.Donath@web.de>
2022-10-27 23:30:00 +0200

    add constants for styles

---
+++ src/main/java/kyle/pdfmanager/constants/StyleConstants.java
@@ -0,0 +1,10 @@
+package kyle.pdfmanager.constants;
+
+public class StyleConstants {
+
+    /**
+     * Style Resource Paths
+     */
+    public static String STYLE_FOLDER = "/styles";
+    public static String WINDOW_STYLE_CLASS = STYLE_FOLDER + "/Window.css";
+}

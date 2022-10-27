2e78ef6a333dfc3e07b78efbcf7f2f4d01cdf448
Ky1le <Doener.Donath@web.de>
2022-10-27 19:05:16 +0200

    spring boot setup
    Initial setup of Spring Boot with Lombok and Maven

---
+++ src/main/java/kyle/pdfmanager/PdfManagerApplication.java
@@ -0,0 +1,13 @@
+package kyle.pdfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfManagerApplication.class, args);
+	}
+
+}
